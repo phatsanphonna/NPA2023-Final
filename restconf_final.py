ae24b6e06fbd85a7072ae53ce2ebe5c1808622b0
phatsanphonna <phatsanphon2004@outlook.com>
2024-10-19 00:11:51 +0700

    feat: enable fn

--- restconf_final.py
+++ restconf_final.py
@@ -22,21 +22,21 @@ def interface_exist():
 
 def create():
     yangConfig = {
-    "ietf-interfaces:interface": {
-        "name": loopback_name,
-        "type": "iana-if-type:softwareLoopback",
-        "enabled": True,
-        "ietf-ip:ipv4": {
-            "address": [
-                {
-                    "ip": "172.30.171.1",
-                    "netmask": "255.255.255.0"
-                }
-            ]
-        },
-        "ietf-ip:ipv6": {}
+        "ietf-interfaces:interface": {
+            "name": loopback_name,
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {
+                "address": [
+                    {
+                        "ip": "172.30.171.1",
+                        "netmask": "255.255.255.0"
+                    }
+                ]
+            },
+            "ietf-ip:ipv6": {}
+        }
     }
-}
     
     if interface_exist():
         return f"Cannot create: Interface loopback {student_id}"
@@ -75,21 +75,28 @@ def delete():
 
 
 def enable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
+    if not interface_exist():
+        return f"Cannot enable: Interface loopback {student_id}"
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "enabled": True,
+        }
+    }
+
+    resp = requests.put(
+        f'{api_url}/interface={loopback_name}', 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
-    if(resp.status_code >= 200 and resp.status_code <= 299):
+    if resp.status_code >= 200 and resp.status_code <= 299:
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+        return f"Interface loopback {student_id} is enabled successfully"
+
+    print('Error. Status Code: {}'.format(resp.status_code))
 
 
 def disable():
